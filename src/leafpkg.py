d174cb613c211b0544edf88a02b5bfebb7f7f8f7
zimsneexh <zimsneexh@gmail.com>
2023-02-19 12:58:00 +0100

    leafpkg.py: Support basedir specification

--- src/leafpkg.py
+++ src/leafpkg.py
@@ -11,9 +11,13 @@ class leafpkg():
         self.description = ""
         self.dependencies = ""
     
-    def write_package_directory(self):
+    def write_package_directory(self, basedir=os.getcwd()):
+
+        print(basedir)
+        input()
+
         pkg_dir = "{}-{}".format(self.name, self.version)
-        pkg_path = os.path.join(os.getcwd(), pkg_dir)
+        pkg_path = os.path.join(basedir, pkg_dir)
 
         # create pkg dir
         try:
